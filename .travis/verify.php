060c789286da3d9154797222f675fe006e5d9213
Evan Mattson <me@aaemnnost.tv>
2019-12-29 20:12:35 +0200

    verify current working dir

--- .travis/verify.php
+++ .travis/verify.php
@@ -3,7 +3,7 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 try {
-    ComposerHash\verify(__DIR__);
+    ComposerHash\verify(getcwd());
     echo "Hashes match.\n";
 } catch (Exception $exception) {
     echo $exception->getMessage() . "\n";
